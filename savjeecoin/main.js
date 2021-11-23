64fe0894357ee0f50541a28f02dc9b824c21d80f
Jubeto Oliveira <jubetooliveira@gmail.com>
2021-11-22 22:00:11 -0300

    Recompensas e Transações
    - Mining rewards & transactions - Blockchain in Javascript (part 3)

--- savjeecoin/main.js
+++ savjeecoin/main.js
@@ -1,17 +1,24 @@
 const SHA256 = require('crypto-js/sha256')
 
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
+    }
+}
+
 class Block{
-    constructor(index, timestamp, data, previousHah = ''){
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = ''){
         this.timestamp = timestamp;
-        this.data = data;
-        this.previousHah = previousHah;
+        this.transactions = transactions;
+        this.previousHash = previousHash;
         this.hash = this.calculateHash();
         this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.previousHah + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
     }
 
     mineBlock(difficulty){
@@ -27,21 +34,51 @@ class Block{
 class BlockChain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
-        this.difficulty = 4;
+        this.difficulty = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock(){
-        return new Block(0, "01/01/2017", "Genesis block", "0");
+        return new Block("01/01/2017", "Genesis block", "0");
     }
 
     getLatestBlock(){
         return this.chain[this.chain.length -1];
     }
 
-    addBlock(newBlock){
-        newBlock.previousHah = this.getLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    minePendingTransactions(miningRewardAddress){
+        let block = new Block(Date.now(), this.pendingTransactions);
+        block.mineBlock(this.difficulty);
+
+        console.log("Block successfully mined!");
+        this.chain.push(block);
+
+        this.pendingTransactions = [
+            new Transaction(null, miningRewardAddress, this.miningReward)
+        ];
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+
+        for(const block of this.chain){
+            for(const trans of block.transactions){
+                if(trans.fromAddress === address){
+                    balance -= trans.amount;
+                }
+
+                if(trans.toAddress === address){
+                    balance += trans.amount;
+                }
+            }
+        }
+
+        return balance;
     }
 
     isChainValid(){
@@ -53,7 +90,7 @@ class BlockChain{
                 return false;
             }
 
-            if(currenBlock.previousHah !== previousBlock.hash){
+            if(currenBlock.previousHash !== previousBlock.hash){
                 return false;
             }
 
@@ -63,10 +100,17 @@ class BlockChain{
 }
 
 let savjeeCoin = new BlockChain();
+savjeeCoin.createTransaction(new Transaction('address1', 'addres2', 100));
+savjeeCoin.createTransaction(new Transaction('address2', 'addres1', 50));
 
-console.log('Mining block 1...');
-savjeeCoin.addBlock(new Block(1, "10/07/2017", { amount: 4}));
+console.log('\n Starting the miner...');
+savjeeCoin.minePendingTransactions('xaviers-address');
 
-console.log('Mining block 2...');
-savjeeCoin.addBlock(new Block(2, "12/07/2017", {amount: 10}));
+console.log('\nBalance of xavier is', savjeeCoin.getBalanceOfAddress('xaviers-address'));
+
+console.log('\n Starting the miner again...');
+savjeeCoin.minePendingTransactions('xaviers-address');
+
+console.log('\nBalance of xavier is', savjeeCoin.getBalanceOfAddress('xaviers-address'));
+
 
