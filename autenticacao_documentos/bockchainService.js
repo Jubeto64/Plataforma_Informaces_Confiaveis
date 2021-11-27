d927b75aeb74f02b2befee26ed2dfc36cdad6792
Jubeto Oliveira <jubetooliveira@gmail.com>
2021-11-27 11:22:31 -0300

    Criando um frontend semelhante ao tutorial
    - definição do blockchainService
    - cabeçalho e cards parecidos com o tutorial

---
+++ autenticacao_documentos/bockchainService.js
@@ -0,0 +1,33 @@
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const{BlockChain, Transaction} = require('./blockchain');
+
+
+class BlockchainService {
+    blockchainInstance = new BlockChain();
+    walletKeys = [];
+  
+    constructor() {
+      this.blockchainInstance.difficulty = 1;
+      this.blockchainInstance.minePendingTransactions('my-wallet-address');
+  
+      this.generateWalletKeys();
+    }
+  
+    getBlocks(){
+      this.blockchainInstance.chain;
+    }
+  
+    generateWalletKeys(){
+      const ec = new EC('secp256k1');
+      const key = ec.genKeyPair();
+  
+      this.walletKeys.push({
+        keyObj: key,
+        publicKey: key.getPublic('hex'),
+        privateKey: key.getPrivate('hex')
+      })
+    }
+}
+
+module.exports.BlockchainService = BlockchainService;
