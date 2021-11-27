8530e068645b458b368a105b11c1de4ca465654a
Jubeto Oliveira <jubetooliveira@gmail.com>
2021-11-27 14:56:18 -0300

    Criando um frontend semelhante ao tutorial
    - Mostrando as transações de um bloco

--- autenticacao_documentos/routes/index.js
+++ autenticacao_documentos/routes/index.js
@@ -6,12 +6,18 @@ const{BlockchainService} = require('../bockchainService');
 
 var router = express.Router();
 
-var BlockchainServiceInstance = new BlockchainService
+var BlockchainServiceInstance = new BlockchainService;
+var selectedBlock = BlockchainServiceInstance.getBlocks()[1];
+
+
+function showTransaction(block){
+  selectedBlock = block;
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let blocks = BlockchainServiceInstance.getBlocks()
-  res.render('index', { blocks });
+  let blocks = BlockchainServiceInstance.getBlocks();
+  res.render('index', { blocks, transactions: selectedBlock.transactions});
 });
 
 module.exports = router;
