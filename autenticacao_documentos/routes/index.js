ae34101167b602330f274912df928f6789c3b958
Jubeto Oliveira <jubetooliveira@gmail.com>
2021-11-28 13:41:11 -0300

    Criando um frontend semelhante ao tutorial
    - Eu não consegui fazer o express redirecionar para o router apenas clicando no bloco, por isso criei um botão que submete um formulário com o objeto do bloco.
    - O processo de transformar em JSON e depois em objeto faz com que as transações percam o método isValid(), que é utilizado na aplicação, por isso eu estou instanciando novas transações com os mesmos atributos toda vez que eu mudo os blocos selecionados

--- autenticacao_documentos/routes/index.js
+++ autenticacao_documentos/routes/index.js
@@ -9,15 +9,24 @@ var router = express.Router();
 var BlockchainServiceInstance = new BlockchainService;
 var selectedBlock = BlockchainServiceInstance.getBlocks()[1];
 
-
-function showTransaction(block){
-  selectedBlock = block;
-}
+let blocks = BlockchainServiceInstance.getBlocks();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let blocks = BlockchainServiceInstance.getBlocks();
   res.render('index', { blocks, transactions: selectedBlock.transactions});
 });
+
+router.post('/', function(req, res, next){
+  selectedBlock = JSON.parse(req.body.block);
+  let transactions = [];
+  let temp_transaction;
+  selectedBlock.transactions.map((item)=>{
+    temp_transaction = new Transaction(item.fromAddress, item.toAddress, item.amount)
+    temp_transaction.timestamp = item.timestamp
+    transactions.push(temp_transaction)
+  })
+  console.log(selectedBlock)
+  res.render('index', { blocks, transactions});
+})
 
 module.exports = router;
